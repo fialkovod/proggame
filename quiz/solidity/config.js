0250d77c9d868976fe3e191c4f0231d397fbba52
fialkovod <info@fialkovod.ru>
2022-11-18 16:53:27 +0300

    added solidity base

---
+++ quiz/solidity/config.js
@@ -0,0 +1,4 @@
+const config = {
+  MAXTASK: 10, // максимальное количество заданий на текущий день
+};
+export default config;
---
+++ quiz/solidity/index.js
@@ -0,0 +1,22 @@
+export const quizes = [
+  {
+    id: 1,
+    pic: "",
+    question: "Solidity - это язык программирования смарт-контрактов?",
+    options: ["Да", "Нет", "Не знаю", "Давайте дальше"],
+    correct_option_id: 0,
+    explanation: "Да, это так",
+    startdif: 1,
+    open_period: 5,
+  },
+  {
+    id: 2,
+    pic: "",
+    question: "Solidity — это язык программирования с открытым исходным кодом?",
+    options: ["Да", "Нет", "Не знаю", "Давайте дальше"],
+    correct_option_id: 0,
+    explanation: "Да, с открытым",
+    startdif: 1,
+    open_period: 5,
+  },
+];
